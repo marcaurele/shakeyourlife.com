d4731c792c742b2f5d85ce5ebbc7dd001da0cedd
Marc-Aurèle Brothier <marc-aurele@peakxl.com>
2013-10-16 15:28:38 +0200

    Theme done

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,26 +21,33 @@ DEFAULT_DATE_FORMAT = '%d %B %Y'
 
 DEFAULT_PAGINATION = 10
 
-SUMMARY_MAX_LENGTH = 30
+SUMMARY_MAX_LENGTH = 50
 
 PAGE_DIR = 'pages'
 PAGE_EXCLUDES = ('articles',)
-PAGE_URL = '{slug}.html'
+PAGE_URL = 'content/{slug}.html'
 PAGE_SAVE_AS = PAGE_URL
-PAGE_LANG_URL = '{slug}-{lang}.html'
+PAGE_LANG_URL = 'content/{slug}-{lang}.html'
 PAGE_LANG_SAVE_AS = PAGE_LANG_URL
 
 ARTICLE_DIR = 'articles'
 ARTICLE_EXCLUDES = ('pages',)
-ARTICLE_URL = '{date:%Y}/{date:%B}/{slug}.html'
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = ARTICLE_URL
-ARTICLE_LANG_URL = '{date:%Y}/{date:%B}/{slug}-{lang}.html'
+ARTICLE_LANG_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}.html'
 ARTICLE_LANG_SAVE_AS = ARTICLE_LANG_URL
-YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%B}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/index.html'
 
-TAG_URL = False
-TAG_SAVE_AS = False
+TAG_URL = 'tag/{slug}.html'
+TAG_SAVE_AS = TAG_URL
+TAGS_URL = False
+TAGS_SAVE_AS = TAGS_URL
+
+AUTHOR_URL = False
+AUTHOR_SAVE_AS = AUTHOR_URL
+AUTHORS_URL = False
+AUTHORS_SAVE_AS = AUTHORS_URL
 
 TYPOGRIFY = False
 
