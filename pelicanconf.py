97f3129338a414868f85df922f09c5222b9a8e4a
Marc-Aurèle Brothier <m@brothier.org>
2024-10-22 21:19:10 +0200

    gitlab ci

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,17 +1,26 @@
-AUTHOR = 'Marc-Aurèle Brothier'
-SITENAME = 'Shake Your Life'
+AUTHOR = "Marc-Aurèle Brothier"
+SITENAME = "Shake Your Life"
 SITEURL = ""
 
-THEME = 'theme_syl'
+THEME = "theme_syl"
 
 PATH = "content"
-PAGE_PATHS = ["pages", ]
-ARTICLE_PATHS = ["articles", ]
+PAGE_PATHS = [
+    "pages",
+]
+ARTICLE_PATHS = [
+    "articles",
+]
+STATIC_PATHS = [
+    "images",
+    "calendar-web",
+    "robots.txt",
+]
 
-TIMEZONE = 'Europe/Rome'
+TIMEZONE = "Europe/Rome"
 
-DEFAULT_LANG = 'en'
-DEFAULT_DATE_FORMAT = '%d %B %Y'
+DEFAULT_LANG = "en"
+DEFAULT_DATE_FORMAT = "%d %B %Y"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,3 +33,5 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+DELETE_OUTPUT_DIRECTORY = True
